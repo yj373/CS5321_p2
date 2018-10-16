1cf503b34fd48aefac350b206bf21fa5acf4c9f9
yanxiaoxing <xy363@cornell.edu>
2018-10-16 16:41:15 -0400

    correct tuple reader

--- src/data/TupleReader.java
+++ src/data/TupleReader.java
@@ -5,7 +5,7 @@ package data;
 import java.io.File;
 
 import java.io.FileNotFoundException;
-
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;  
 import java.nio.channels.FileChannel;
@@ -26,7 +26,9 @@ public class TupleReader {
 	private int attributeNumber;
 	/*the number of tuples we can read in the buffer or page*/
 	private int tupleNumber;
-	
+	/*the file channel we are using*/
+	private FileChannel fcin;
+
 	/*for tuple in this table*/
 	private String tableInfo;
 	private String tableName;
@@ -43,14 +45,47 @@ public class TupleReader {
 			empty = true;
 			filePosition = 0;
 			bufferPosition = 0;
-			this.tableInfo = new String(tableInfo);
+			this.tableInfo = tableInfo;
 			System.out.println("这里的table参数是"+tableInfo);
 			/*create a new buffer with size 4096 bytes*/ 
 			buffer = ByteBuffer.allocate(4096); 
+			/*open file channel*/
+			initFileChannel (tableInfo);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+
+
+	private void initFileChannel (String tableInfo) throws Exception {
+
+		//get the path of file -- correct later
+		//String infile = "/Users/yanxiaoxing/Desktop/CS4321/project 2/samples/input/db/data/Boats";  
+
+		String[] aimTable = tableInfo.split("\\s+");
+		//这个是处理什么corner case的
+		if (aimTable.length<1) {
+			tableName = null;
+		}
+
+		/*get the information of table*/
+		//需要设成全局变量吗
+		tableName = aimTable[0];
+		tableAddress = DataBase.getInstance().getAddresses(tableName);
+		tableAliase = aimTable[aimTable.length-1];
+		attributes = DataBase.getInstance().getSchema(tableName);
+		File tableFile = new File(tableAddress);
+
+		try {
+			/*get the channel of source file*/ 
+			fcin = new RandomAccessFile(tableFile, "r").getChannel();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+
 	}
 
 
@@ -64,12 +99,12 @@ public class TupleReader {
 
 	}
 
-	
+
 	public Tuple readNextTuple() throws Exception {
 
 		/*if buffer is empty, it needs to load another page*/
 		if (empty) {
-			int r = readFromChannel(tableInfo);
+			int r = readFromChannel();
 			// read方法返回读取的字节数，可能为零，如果该通道已到达流的末尾，则返回-1  
 			/*if we have reached the end of file*/
 			if (r == -1) {
@@ -87,11 +122,11 @@ public class TupleReader {
 			data[i] = (long)buffer.getInt(bufferPosition);
 			bufferPosition += 4;
 		}
-		
+
 		/*create a new tuple*/
 		Tuple tuple = new Tuple(data, tableAliase, attributes);
 		tupleNumber--;
-		
+
 		/* after reading all tuples in one page
 		 * load another page to buffer
 		 **/
@@ -119,71 +154,67 @@ public class TupleReader {
 		}
 	}
 
-	
+
 	/**
 	 * load one page to buffer 
 	 * 
-	 * @param tableInfo
-	 * @return
+	 * 
+	 * @return r
 	 * @throws Exception
 	 */
-	private int readFromChannel (String tableInfo) throws Exception  {
+	private int readFromChannel() throws Exception  {
 
-		int[] r = new int[1];
+		// clear方法重设缓冲区，使它可以接受读入的数据  
+		buffer.clear();  
 
-		//get the path of file -- correct later
-		//String infile = "/Users/yanxiaoxing/Desktop/CS4321/project 2/samples/input/db/data/Boats";  
 
-		String[] aimTable = tableInfo.split("\\s+");
-		//这个是处理什么corner case的
-		if (aimTable.length<1) {
-			tableName = null;
-		}
+		System.out.println("读之前这里的file position是"+filePosition);
 
-		/*get the information of table*/
-		tableName = aimTable[0];
-		tableAddress = DataBase.getInstance().getAddresses(tableName);
-		tableAliase = aimTable[aimTable.length-1];
-		attributes = DataBase.getInstance().getSchema(tableName);
-		File tableFile = new File(tableAddress);
-		
+		/*Reads a sequence of bytes from this channel into the given buffer.*/
+		int r = fcin.read(buffer); 
+
+
+
+		/*record the next position*/
+		filePosition = fcin.position()+1;//debug later 不确定这里的position是否是对的
+		System.out.println("读之后这里的file position是"+filePosition);
+
+		/*set the buffer position to zero*/
+		buffer.flip();
+
+		return r;
+
+	}
+
+
+
+	/**
+	 * close the file channel
+	 * 
+	 * @throws IOException
+	 */
+	public void close() throws IOException {
+
 		try {
+			if(fcin != null) {
+				fcin.close();
+			}
 
-			/*get the channel of source file*/ 
-			FileChannel fcin = new RandomAccessFile(tableFile, "r").getChannel();
-	
-			// clear方法重设缓冲区，使它可以接受读入的数据  
-			buffer.clear();  
-
-			/*Reads a sequence of bytes from this channel into the given buffer.*/
-			System.out.println("读之前这里的position是"+filePosition);
-			
-			fcin.position(filePosition);
-			r[0] = fcin.read(buffer); 
-			/*record the next position*/
-			filePosition = fcin.position()+1;//debug later 不确定这里的position是否是对的
-			
-			System.out.println("读之后这里的position是"+filePosition);
-			
-			
-			fcin.close();
-			buffer.flip();//将position置0
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			e.getMessage();
+		} catch(IOException e) {
+			throw e;
 		}
 
-		return r[0];
 	}
 
-
-
-	public void close() {
-
-	}
-
+	
+	/**
+	 * 
+	 */
 	public void reset() {
+
+	}
+	
+	public void reset(int index) {
 
 	}
 
