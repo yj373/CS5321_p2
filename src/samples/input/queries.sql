5007b80c5d1e841da88140f1117bf30f4fc1b8b6
Yixuan Jiang <yixuanjiang@dhcp-rhodes-2951.eduroam.cornell.edu>
2018-10-17 11:03:10 -0400

    deal with concurrent modification exception in logical query plan builder

--- src/samples/input/queries.sql
+++ src/samples/input/queries.sql
@@ -6,3 +6,4 @@ SELECT Reserves.G, Reserves.H FROM Reserves;
 SELECT Sailors.A FROM Sailors WHERE Sailors.B >= Sailors.C
 SELECT Sailors.A FROM Sailors WHERE Sailors.B >= Sailors.C AND Sailors.B < Sailors.C;
 SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;
+SELECT * FROM Sailors, Reserves, Boats WHERE Sailors.A = Reserves.G AND Reserves.H = Boats.D;
