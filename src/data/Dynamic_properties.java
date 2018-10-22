f4adc3f304031c3d63e54788d2858d7ef058a18a
Yixuan Jiang <yixuanjiang@dhcp-rhodes-2183.eduroam.cornell.edu>
2018-10-22 19:44:29 -0400

    temp directory

--- src/data/Dynamic_properties.java
+++ src/data/Dynamic_properties.java
@@ -16,14 +16,30 @@ public class Dynamic_properties {
 	public static String queryPath = inputPath + "/queries.sql";
 	public static String schemaPath = inputPath + "/db/schema.txt";
 	public static String outputPath = "src/samples/output";
+	public static String tempPath = "src/samples/temp";
 	
+	/**
+	 * set input and output path according to pass in parameters
+	 * @param p0 input absolute path
+	 * @param p1 output absolute path
+	 * @param p2 temporary scratch file directory
+	 */
+	public static void setPath(String p0, String p1, String p2) {
+		
+		inputPath = p0;
+		outputPath = p1;
+		tempPath = p2;
+		dataPath = inputPath + "/db/data/";
+		queryPath = inputPath + "/queries.sql";
+		schemaPath = inputPath + "/db/schema.txt";
+		
+	}
 	/**
 	 * set input and output path according to pass in parameters
 	 * @param p0 input absolute path
 	 * @param p1 output absolute path
 	 */
 	public static void setPath(String p0, String p1) {
-		
 		inputPath = p0;
 		outputPath = p1;
 		dataPath = inputPath + "/db/data/";
@@ -31,5 +47,4 @@ public class Dynamic_properties {
 		schemaPath = inputPath + "/db/schema.txt";
 		
 	}
-	
 }
---
+++ src/operators/ExternalSort.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class ExternalSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
